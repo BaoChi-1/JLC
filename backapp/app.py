9b7d84981e3c5b0efe81ebe0764f56ff415a7bbf
Никита Кремнёв <nkremnev3003@gmail.com>
2024-05-21 11:38:46 +0300

    Рабочй билд

--- backapp/app.py
+++ backapp/app.py
@@ -2,9 +2,13 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import json
 import requests
+import logging
 
 app = Flask(__name__)
 CORS(app)
+
+# Настройка логирования
+logging.basicConfig(level=logging.DEBUG)
 
 @app.route("/calculate-internet-pay", methods=["POST"])
 def calculate_internet_pay():
@@ -14,7 +18,7 @@ def calculate_internet_pay():
     try:
         # Получение данных из запроса JSON от клиента
         payload = request.json
-        print(payload)
+        logging.debug(f"Payload received: {payload}")
 
         headers = {
             "Content-Type": "application/json"
@@ -24,6 +28,8 @@ def calculate_internet_pay():
         response = requests.post(url, json=payload, headers=headers)
         response.raise_for_status()
         response_json = response.json()
+        logging.debug(f"Response from JLCPCB: {response_json}")
+
         internet_pay_total_money = response_json.get("carriageSystemMoneyVO", {}).get("internetPayTotalmoney")
 
         if internet_pay_total_money is not None:
@@ -32,6 +38,8 @@ def calculate_internet_pay():
             exchange_rate_response.raise_for_status()
             exchange_rate_data = exchange_rate_response.json()
             exchange_rate = exchange_rate_data["rates"]["RUB"]
+            logging.debug(f"Exchange rate data: {exchange_rate_data}")
+
             converted_amount_rub = internet_pay_total_money * exchange_rate
 
             package_mass = calculate_package_mass(payload)
@@ -43,9 +51,11 @@ def calculate_internet_pay():
                 "packageMass": package_mass
             })
         else:
+            logging.error("Поле internetPayTotalmoney не найдено в ответе.")
             return jsonify({"error": "Поле internetPayTotalmoney не найдено в ответе."}), 400
 
     except (requests.RequestException, json.JSONDecodeError) as e:
+        logging.exception("Ошибка при отправке запроса или обработке JSON")
         return jsonify({"error": f"Ошибка при отправке запроса или обработке JSON: {e}"}), 500
 
 def calculate_package_mass(payload):
@@ -64,6 +74,7 @@ def calculate_package_mass(payload):
     plate_layer = int(payload.get("stencilLayer"))
     plate_counts = payload.get("stencilCounts")
     plate_ply = float(payload.get("stencilPly"))
+
     # Площадь одной платы в квадратных сантиметрах
     plate_area = plate_length * plate_width
     # Масса одной платы в граммах
--- jlc-project/src/components/Form/SideForm/SideForm.js
+++ jlc-project/src/components/Form/SideForm/SideForm.js
@@ -16,16 +16,22 @@ function SideForm({ saveJsonToFile, result }) {
         <div className='el-detail'>
           <h2 className='fonts'>{t('price')}</h2>
           <span>{result && result.internetPayTotalmoney} ¥</span>
-          <p>Converted amount (RUB): {result && result.convertedAmountRub}</p>
+          <p>В рублях (RUB): {result && result.convertedAmountRub}</p>
           <h2 className='fonts'>{t('shippingEstimate')}</h2>
           <h2>{t('weight')}</h2>
-          <span>{result && result.packageMass} g</span>
+          <span>{result && result.packageMass} грамм</span>
         </div>
-        <div className="file-attachment">
-        <div>
-          <label htmlFor="gerberFileInput" className="file-label">{t('Прикрепить gerber файл:')}</label>
-        </div>
-          <input id="gerberFileInput" type="file" accept=".zip,.rar" onChange={handleFileChange} />
+        <div className="file-attachment" style={{ marginBottom: '20px' }}>
+          <div>
+            <label htmlFor="gerberFileInput" className="file-label">{t('Добавить gerber файл:')}</label>
+          </div>
+          <input
+            id="gerberFileInput"
+            type="file"
+            accept=".zip,.rar"
+            onChange={handleFileChange}
+            lang="en"
+          />
         </div>
         <button className="sub" type="submit" onClick={saveJsonToFile}>{t('addCart')}</button>
       </div>
